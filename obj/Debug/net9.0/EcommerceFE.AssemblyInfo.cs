6238d4d2c36d9f9aa272ac02fb210754a80411c5
Chambers <2391439@cognizant.com>
2025-03-30 03:41:45 +0100

    styleUpdate

--- obj/Debug/net9.0/EcommerceFE.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcommerceFE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceFE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c040b5413e4036d2f5c6dd028914a10aff8a3862")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+845ebdc8944698c7704b9bc891202ab1cabc2a3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceFE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceFE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
